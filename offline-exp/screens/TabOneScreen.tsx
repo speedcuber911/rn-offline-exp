b4297135288921762c1fd0494450f42c500571ca
dibyajyoti <dibyajyotipradhanoffice@gmail.com>
2020-07-04 19:41:12 +0530

    Added TDetails Page and passed data from nav props

--- offline-exp/screens/TabOneScreen.tsx
+++ offline-exp/screens/TabOneScreen.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import TSummary from '../components/TSummary';
 import TList from '../components/TList';
 
-export default function TabOneScreen() {
-  return (
+export default function TabOneScreen(props: any) {
+    return (
     <View style={styles.container}>
       <TSummary/>
-      <TList/>
+      <TList navigation={props.navigation}/>
     </View>
   );
 }
@@ -18,14 +16,5 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
